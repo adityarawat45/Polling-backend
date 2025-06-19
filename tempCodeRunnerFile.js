99e7b79b709153e4ba043a0eec5585deee667f5f
Aditya Rawat <rawataditya193@gmail.com>
2025-06-20 02:17:04 +0530

    Initiazlied backend server

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,16 +1,14 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
 const mongoose = require("mongoose");
 const app = express();
+const env = require("dotenv").config();
 
 app.use(cors())
 app.use(express.json());
-app.listen(3000)
 
 console.log("Port we are getting : ", process.env.PORT)
-
 
 
