1258e4681dc7abcd12bb59b7942fb01c697dc378
Aditya Rawat <rawataditya193@gmail.com>
2026-01-03 01:28:46 +0530

    Server init

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 import http from "http";
 import dotenv from "dotenv";
 import app from "./app.js";
-import connectDB from "./config/db.js";
-import initSocket from "./config/socket.js";
-
+// import connectDB from "./config/db.js";
+// import initSocket from "./config/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 
-connectDB(); //db
-initSocket(server); //socket init
+// connectDB(); //db
+// initSocket(server); //socket inita
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
